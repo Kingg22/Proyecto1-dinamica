1e88a276550f9de22d5964550c15f424a48a7643
Kingg22 <rey2213@outlook.com>
2023-11-15 10:03:13 -0500

    ponerlo bonito

--- src/clases/Circular.java
+++ src/clases/Circular.java
@@ -3,19 +3,55 @@ package clases;
 import javax.swing.JOptionPane;
 
 public class Circular {
-    Nodo front;
-    Nodo back;
+    Nodo front, back;
     String ColaCircular = "";
 
     public Circular () {
         front = back = null;
     }
+    
+    //Método para saber si la cola esta vacia
     public boolean ColaCircularVacia() {
         if (front == back && front == null) {
             return true;
         } else 
         return false;
     }
+
+    //Método para insertar a la cola
+    public void Insertar (int informacion) {
+        Nodo auxiliar = new Nodo(informacion);
+
+        if (ColaCircularVacia()) {
+            front = auxiliar;
+            back = auxiliar;
+            auxiliar.siguiente = front;
+        } else {
+            back.siguiente = auxiliar;
+            back = auxiliar;
+            auxiliar.siguiente = front;
+        }
+    }
+
+    //Método para extraer de la cola
+    public int Eliminar() {
+        if (!ColaCircularVacia()) {
+            int informacion = front.informacion;
+
+            if (front == back) {
+                front = back = null;
+            } else {
+                front = front.siguiente;
+                informacion = front.informacion;
+                back.siguiente = front.siguiente;
+            }
+            return informacion;
+        } else {
+            return Integer.MIN_VALUE;
+        }
+    }
+    
+    //Método para mostrar el contenido de la cola
     public void imprimirColaCircular() {
         if (ColaCircularVacia()) {
         Nodo recorrido = front;
@@ -44,34 +80,6 @@ public class Circular {
             JOptionPane.PLAIN_MESSAGE);
         }
         ColaCircular = "";
-        }
-    }
-    public void Insertar (int informacion) {
-        Nodo auxiliar = new Nodo(informacion);
-        if (ColaCircularVacia()) {
-            front = auxiliar;
-            back = auxiliar;
-            auxiliar.siguiente = front;
-        } else {
-            back.siguiente = auxiliar;
-            auxiliar.siguiente = front;
-            back = auxiliar;
-        }
-    }
-    public int Eliminar() {
-        int informacion = 0;
-        if (!ColaCircularVacia()) {
-            if (front == back) {
-                informacion = front.informacion;
-                front = back = null;
-            } else {
-                informacion = front.informacion;
-                back.siguiente = front.siguiente;
-                front = front.siguiente;
-            }
-        return informacion;
-        } else {
-            return Integer.MIN_VALUE;
         }
     }
 }
